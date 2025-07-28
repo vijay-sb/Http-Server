f00c15e0ada2569d9c07dd2c562d811321ec102a
vijaysb0613 <vijaysb2006@gmail.com>
2025-07-28 22:15:25 +0530

    handle content from req

--- app/main.go
+++ app/main.go
@@ -44,16 +44,28 @@ func main() {
   parts := strings.Split(string(buff),CRLF)
 
   lineparts := strings.Split(parts[0]," ")
-  if lineparts[1] != "/" {
-    conn.Write([]byte("http/1.1 404 not found\r\n\r\n"))
+  if lineparts[1] == "/"  {
+    conn.Write([]byte("http/1.1 200 OK\r\n\r\n"))
+    conn.Close()
+  } else if strings.HasPrefix(lineparts[1],"/echo") {
+    newParts := strings.Split(lineparts[1],"/")
+    if len(newParts) > 3 {
+      conn.Write([]byte("http/1.1 404 not found\r\n\r\n"))
+    conn.Close()
+    }
+    text := newParts[2]
+    textlength := len(newParts[2])
+
+    conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",textlength,text)))
     conn.Close()
 
+ 
+  } else {
+conn.Write([]byte("http/1.1 404 not found\r\n\r\n"))
+    conn.Close()
   }
-  
-   conn.Write([]byte("http/1.1 200 not OK\r\n\r\n"))
-    conn.Close()
 
-  
+   
 
 
 
