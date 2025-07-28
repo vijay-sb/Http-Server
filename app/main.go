8dc0beb1cf7f25d3ac46619d6b221b6797814764
vijaysb0613 <vijaysb2006@gmail.com>
2025-07-28 23:17:00 +0530

    Header Handlers

--- app/main.go
+++ app/main.go
@@ -16,6 +16,7 @@ const CRLF = "\r\n"
 func main() {
 	// you can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("logs from your program will appear here!")
+
 	//
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
@@ -41,6 +42,21 @@ func main() {
 	parts := strings.Split(string(buff), CRLF)
 
 	lineparts := strings.Split(parts[0], " ")
+
+	header := make(map[string]string)
+	for i := 1; i < len(parts); i++ {
+		lines := parts[i]
+		if lines == " " {
+			break
+		}
+		headerParts := strings.SplitN(lines, ": ", 2)
+		if len(headerParts) == 2 {
+			key := headerParts[0]
+			value := headerParts[1]
+			header[key] = value
+		}
+	}
+
 	if lineparts[1] == "/" {
 		conn.Write([]byte("http/1.1 200 OK\r\n\r\n"))
 		fmt.Println(("These is /"))
@@ -48,15 +64,20 @@ func main() {
 	} else if strings.HasPrefix(lineparts[1], "/echo") {
 		newParts := strings.Split(lineparts[1], "/")
 		if len(newParts) > 3 {
-			conn.Write([]byte("http/1.1 404 not found\r\n\r\n"))
+			conn.Write([]byte("http/1.1 404 Not Found\r\n\r\n"))
 		}
 		text := newParts[2]
 		textlength := len(newParts[2])
 
 		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", textlength, text)))
 
+	} else if strings.HasPrefix(lineparts[1], "/user-agent") {
+		content := header["User-Agent"]
+		contentlen := len(content)
+		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", contentlen, content)))
+
 	} else {
 		conn.Write([]byte("http/1.1 404 not found\r\n\r\n"))
-		conn.Close()
+
 	}
 }
