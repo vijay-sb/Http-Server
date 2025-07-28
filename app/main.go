5ec14b2037e33c6e323fe56b2e41d1262b8533d6
vijaysb0613 <vijaysb2006@gmail.com>
2025-07-28 22:33:19 +0530

    comment out stage 1

--- app/main.go
+++ app/main.go
@@ -4,73 +4,59 @@ import (
 	"fmt"
 	"net"
 	"os"
-  "strings"
+	"strings"
 )
 
 // ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
 var _ = net.Listen
 var _ = os.Exit
+
 const CRLF = "\r\n"
 
 func main() {
 	// you can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("logs from your program will appear here!")
-
-	// uncomment this block to pass the first stage
 	//
-	 l, err := net.Listen("tcp", "0.0.0.0:4221")
-	 if err != nil {
-	 	fmt.Println("failed to bind to port 4221")
-	 	os.Exit(1)
-	 }
-	
-  conn, err := l.Accept()
-	 if err != nil {
-	 	fmt.Println("error accepting connection: ", err.Error())
-	 	os.Exit(1)
-	 }
-     
-  msg := []byte("HTTP/1.1 200 OK\r\n\r\n")
-   conn.Write(msg)
-  
-  conn.Close()
-  
+	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	if err != nil {
+		fmt.Println("failed to bind to port 4221")
+		os.Exit(1)
+	}
 
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println("error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
 
-  //server will extract the URL path from an HTTP request, and respond with either a 200 or 404, depending on the path.
-  buff := make([]byte,1024)
-  conn.Read(buff)
+	// msg := []byte("HTTP/1.1 200 OK\r\n\r\n")
+	// conn.Write(msg)
 
-  parts := strings.Split(string(buff),CRLF)
+	// conn.Close()
 
-  lineparts := strings.Split(parts[0]," ")
-  if lineparts[1] == "/"  {
-    conn.Write([]byte("http/1.1 200 OK\r\n\r\n"))
-    conn.Close()
-  } else if strings.HasPrefix(lineparts[1],"/echo") {
-    newParts := strings.Split(lineparts[1],"/")
-    if len(newParts) > 3 {
-      conn.Write([]byte("http/1.1 404 not found\r\n\r\n"))
-    conn.Close()
-    }
-    text := newParts[2]
-    textlength := len(newParts[2])
+	//server will extract the URL path from an HTTP request, and respond with either a 200 or 404, depending on the path.
+	buff := make([]byte, 1024)
+	conn.Read(buff)
 
-    conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",textlength,text)))
-    conn.Close()
+	parts := strings.Split(string(buff), CRLF)
 
- 
-  } else {
-conn.Write([]byte("http/1.1 404 not found\r\n\r\n"))
-    conn.Close()
-  }
+	lineparts := strings.Split(parts[0], " ")
+	if lineparts[1] == "/" {
+		conn.Write([]byte("http/1.1 200 OK\r\n\r\n"))
+		fmt.Println(("These is /"))
 
-   
+	} else if strings.HasPrefix(lineparts[1], "/echo") {
+		newParts := strings.Split(lineparts[1], "/")
+		if len(newParts) > 3 {
+			conn.Write([]byte("http/1.1 404 not found\r\n\r\n"))
+		}
+		text := newParts[2]
+		textlength := len(newParts[2])
 
+		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", textlength, text)))
 
-
-  
-
-
-
+	} else {
+		conn.Write([]byte("http/1.1 404 not found\r\n\r\n"))
+		conn.Close()
+	}
 }
