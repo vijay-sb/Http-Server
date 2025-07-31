84ede307380ac09687bff66bee88ef97527336e4
vijaysb0613 <vijaysb2006@gmail.com>
2025-07-31 11:37:27 +0530

    Handle POST req

--- app/main.go
+++ app/main.go
@@ -9,7 +9,6 @@ import (
 	"unicode/utf8"
 )
 
-
 var _ = net.Listen
 var _ = os.Exit
 
@@ -18,7 +17,6 @@ const CRLF = "\r\n"
 func main() {
 	fmt.Println("logs from your program will appear here!")
 
-	
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("failed to bind to port 4221")
@@ -42,29 +40,28 @@ func main() {
 
 // server will extract the URL path from an HTTP request, and respond with either a 200 or 404, depending on the path.
 func Handlereq(conn net.Conn) {
-	tempDirectory := "temp";
+	tempDirectory := "temp"
 	buff := make([]byte, 1024)
 	conn.Read(buff)
 
 	parts := strings.Split(string(buff), CRLF)
-    len_of_parts := len(parts)
+	len_of_parts := len(parts)
 	lineparts := strings.Split(parts[0], " ")
 	method := lineparts[0]
 	body_content := parts[len_of_parts-1]
-
 
 	header := make(map[string]string)
 	type HTTPRequest struct {
 		Headers map[string]string
 		Url     string
 		Method  string
-		Body	string
- 	}
+		Body    string
+	}
 	request := HTTPRequest{
 		Url:     lineparts[1],
 		Headers: header,
-		Method: method,
-		Body: body_content,
+		Method:  method,
+		Body:    body_content,
 	}
 	for i := 1; i < len(parts); i++ {
 		lines := parts[i]
@@ -98,28 +95,42 @@ func Handlereq(conn net.Conn) {
 		contentlen := len(content)
 		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", contentlen, content)))
 
-	}else if strings.HasPrefix(request.Url,"/files"){
-		fileparts := strings.Split(request.Url,"/")
-		
+	} else if strings.HasPrefix(request.Url, "/files") {
+		fileparts := strings.Split(request.Url, "/")
+
 		filename := fileparts[2]
 		filepath := fmt.Sprintf("%s/%s", tempDirectory, filename)
-
 
 		if len(fileparts) > 3 {
 			conn.Write([]byte("http/1.1 404 Not Found\r\n\r\n"))
 			return
 		}
 
-		if _,err := os.Stat(filepath); errors.Is(err, os.ErrNotExist) {
+		if _, err := os.Stat(filepath); errors.Is(err, os.ErrNotExist) {
 			conn.Write([]byte("http/1.1 404 Not Found\r\n\r\n"))
 			return
 		}
-		content,_ := os.ReadFile(filepath)
+		content, _ := os.ReadFile(filepath)
 		contentLength := utf8.RuneCountInString(string(content))
 		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", contentLength, content)))
-	}else if  request.Method == "POST"{
-		
-
+	} else if request.Method == "POST" {
+		fileparts := strings.Split(request.Url, "/")
+		filname := fileparts[2]
+		filepath := fmt.Sprintf("%s/%s", tempDirectory, filname)
+		if len(fileparts) > 3 {
+			conn.Write([]byte("http/1.1 404 Not Found\r\n\r\n"))
+			return
+		}
+		if _, err := os.Stat(filepath); errors.Is(err, os.ErrNotExist) {
+			conn.Write([]byte("http/1.1 404 Not Found\r\n\r\n"))
+			return
+		}
+		content := request.Body
+		if err := os.WriteFile(filepath, []byte(content), 0644); err == nil {
+			conn.Write([]byte("http/1.1 404 Not Found\r\n\r\n"))
+			return
+		}
+		conn.Write([]byte("http/1.1 201 Created \r\n\\r\n"))
 	} else {
 		conn.Write([]byte("http/1.1 404 not found\r\n\r\n"))
 
